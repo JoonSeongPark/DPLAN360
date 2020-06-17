fc126d728260535e31e468175117a986ac2db540
JoonSeongPark <rytt@yonsei.ac.kr>
2020-06-17 14:32:45 +0900

    media model update

--- models/medium.js
+++ models/medium.js
@@ -2,54 +2,60 @@ const Sequelize = require("sequelize");
 
 const sequelize = require("../util/database");
 
-const Medium = sequelize.define("medium", {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true,
+const Medium = sequelize.define(
+  "medium",
+  {
+    id: {
+      type: Sequelize.INTEGER,
+      autoIncrement: true,
+      allowNull: false,
+      primaryKey: true,
+    },
+    name: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    pay_condition: {
+      type: Sequelize.STRING,
+    },
+    bill_type: {
+      type: Sequelize.STRING,
+    },
+    provide_fee_rate: {
+      type: Sequelize.STRING,
+    },
+    bill_publisher: {
+      type: Sequelize.STRING,
+    },
+    memo: {
+      type: Sequelize.STRING(1000),
+    },
+    inter_type: {
+      type: Sequelize.STRING,
+    },
+    inter_name: {
+      type: Sequelize.STRING,
+    },
+    agency_fee_rate: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+    },
+    media_fee_rate: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+    },
+    dplan_fee_rate: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+    },
+    inter_fee_rate: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+    },
   },
-  title: {
-    type: Sequelize.STRING,
-    allowNull: false,
-  },
-  pay_condition: {
-    type: Sequelize.STRING,
-  },
-  bill_type: {
-    type: Sequelize.STRING,
-  },
-  media_provide_fee: {
-    type: Sequelize.STRING,
-  },
-  bill_pubilsher: {
-    type: Sequelize.STRING,
-  },
-  memo: {
-    type: Sequelize.STRING(1000),
-  },
-  inter_type: {
-    type: Sequelize.STRING,
-  },
-  inter_name: {
-    type: Sequelize.STRING,
-  },
-  agency_fee_rate: {
-    type: Sequelize.INTEGER,
-    allowNull: false,
-  },
-  media_fee_rate: {
-    type: Sequelize.INTEGER,
-    allowNull: false,
-  },
-  dplan_fee_rate: {
-    type: Sequelize.INTEGER,
-    allowNull: false,
-  },
-  inter_fee_rate: {
-    type: Sequelize.INTEGER,
-    allowNull: false,
-  },
-});
+  {
+    timestamps: false,
+  }
+);
 
 module.exports = Medium;
