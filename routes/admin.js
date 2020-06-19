6022c6919f88af09a282e6267e19e70e0711263c
JoonSeongPark <rytt@yonsei.ac.kr>
2020-06-19 22:41:36 +0900

    category router complited

--- routes/admin.js
+++ routes/admin.js
@@ -4,6 +4,9 @@ const router = express.Router();
 
 const adminMediaController = require("../controllers/admin/media");
 const adminAgencyController = require("../controllers/admin/agency");
+const adminAdvertiserController = require("../controllers/admin/advertiser");
+
+const categoryController = require("../controllers/admin/category");
 
 router.get("/media", adminMediaController.getMedia);
 router.get("/add-media", adminMediaController.getAddMedia);
@@ -18,5 +21,26 @@ router.post("/add-agency", adminAgencyController.postAddAgency);
 router.get("/edit-agency/:agencyId", adminAgencyController.getEditAgency);
 router.post("/edit-agency", adminAgencyController.postEditAgency);
 router.post("/delete-agency", adminAgencyController.postDeleteAgency);
+
+router.get("/advertisers", adminAdvertiserController.getAdvertisers);
+router.get("/add-advertiser", adminAdvertiserController.getAddAdvertiser);
+router.post("/add-advertiser", adminAdvertiserController.postAddAdvertiser);
+// router.get("/edit-advertiser/:advertiserId", adminAdvertiserController.getEditAdvertiser);
+// router.post("/edit-advertiser", adminAdvertiserController.postEditAdvertiser);
+router.post(
+  "/delete-advertiser",
+  adminAdvertiserController.postDeleteAdvertiser
+);
+
+router.get("/categories", categoryController.getCategories);
+router.get("/add-category", categoryController.getAddCategory);
+router.post("/add-main-category", categoryController.postAddMain);
+router.get("/edit-main-category/:mainId", categoryController.getEditMain);
+router.post("/edit-main-category", categoryController.postEditMain);
+router.post("/delete-main-category", categoryController.postDeleteMain);
+router.post("/add-sub-category", categoryController.postAddSub);
+router.get("/edit-sub-category/:subId", categoryController.getEditSub);
+router.post("/edit-sub-category", categoryController.postEditSub);
+router.post("/delete-sub-category", categoryController.postDeleteSub);
 
 module.exports = router;
