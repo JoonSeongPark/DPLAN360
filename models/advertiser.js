6f26ef9fb13747c9e5a9cf35a562d801dac74df8
JoonSeongPark <rytt@yonsei.ac.kr>
2020-06-20 14:32:22 +0900

    advertiser db model setting

--- models/advertiser.js
+++ models/advertiser.js
@@ -2,25 +2,31 @@ const Sequelize = require("sequelize");
 
 const sequelize = require("../util/database");
 
-const Advertiser = sequelize.define("advertiser", {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true,
+const Advertiser = sequelize.define(
+  "advertiser",
+  {
+    id: {
+      type: Sequelize.INTEGER,
+      autoIncrement: true,
+      allowNull: false,
+      primaryKey: true,
+    },
+    name: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    main_category: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    sub_category: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
   },
-  name: {
-    type: Sequelize.STRING,
-    allowNull: false
-  },
-  main_category: {
-    type: Sequelize.STRING,
-    allowNull: false,
-  },
-  sub_category: {
-    type: Sequelize.STRING,
-    allowNull: false,
+  {
+    timestamps: false,
   }
-});
+);
 
 module.exports = Advertiser;
