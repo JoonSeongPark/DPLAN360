c991b7df5b55e00df503c193b6e143eb9688b7ce
JoonSeongPark <rytt@yonsei.ac.kr>
2020-06-23 16:00:22 +0900

    404 router & authentication updated

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
 const path = require("path");
 
 const express = require("express");
+const session = require("express-session");
+const MySQLStore = require("express-mysql-session")(session);
+
+const csrf = require("csurf");
+const flash = require("connect-flash");
+const bcrypt = require("bcryptjs");
+
+const errorController = require("./controllers/error");
 
 const app = express();
+
+const store = new MySQLStore({
+  host: "localhost",
+  port: "3306",
+  user: "root",
+  password: "Pjs895623!",
+  database: "dplan360",
+  clearExpired: true,
+});
+const csrfProtection = csrf();
 
 const sequelize = require("./util/database");
 
@@ -21,13 +39,49 @@ app.set("views", "views");
 
 const adminRoutes = require("./routes/admin");
 const workRoutes = require("./routes/work");
+const authRoutes = require("./routes/auth");
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
+app.use(
+  session({
+    secret: "dplandplan",
+    resave: false,
+    saveUninitialized: false,
+    store: store,
+  })
+);
+app.use(csrfProtection);
+app.use(flash());
+
+app.use((req, res, next) => {
+  if (req.session.user) return next();
+
+  User.findByPk(req.session.user._id)
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((err) => {
+      return console.log(err);
+    });
+});
+
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.session.isLoggedIn;
+  res.locals.csrfToken = req.csrfToken();
+  next();
+});
+
+// routes
 app.use("/admin", adminRoutes);
 app.use(workRoutes);
+app.use(authRoutes);
 
+app.use(errorController.get404);
+
+// entity relations
 User.belongsTo(Team);
 Team.hasMany(User);
 
@@ -51,16 +105,24 @@ sequelize
   })
   .then((team) => {
     if (!team) {
-      Team.create({ name: "김나영" }).then((team) => {
-        team.createUser({
-          name: "박준성",
-          email: "rytt@yonsei.ac.kr",
-          password: "123",
-        });
-      });
+      Team.create({ name: "김나영" });
       Team.create({ name: "조선영" });
       Team.create({ name: "이미선" });
     }
+    return User.findOne({ name: "관리자" });
+  })
+  .then((user) => {
+    if (!user) {
+      return bcrypt.hash("123", 12).then((hashedPassword) => {
+        const admin = new User({
+          name: "관리자",
+          email: "admin@d-plan360.com",
+          password: hashedPassword,
+        });
+        return admin.save();
+      });
+    }
+    return user;
   })
   .then((user) => {
     app.listen(3000);
