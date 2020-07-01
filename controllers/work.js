8579cb470ee5cc0d63ed8afabb628c412fafca70
JoonSeongPark <rytt@yonsei.ac.kr>
2020-07-02 00:34:51 +0900

    create mediaItem part update

--- controllers/work.js
+++ controllers/work.js
@@ -134,6 +134,7 @@ exports.getAddCampaign = (req, res, next) => {
 };
 
 exports.postAddCampaign = (req, res, next) => {
+  console.log(req.body)
   const {
     // 캠페인 정보
     cam_type,
@@ -148,12 +149,17 @@ exports.postAddCampaign = (req, res, next) => {
     cam_agency_fee,
     cam_tax_month,
     media_issue_type,
-    // 매체 정보
-    media_name,
+    _csrf,
+    media_count,
+  } = req.body;
+  // 매체 정보
+  let {
+    media_id,
     media_start,
     media_end,
     lower_inter_type,
     lower_inter_name,
+    lower_issue_date,
     lower_issue_type,
     lower_agency_fee_rate,
     lower_media_fee_rate,
@@ -166,28 +172,83 @@ exports.postAddCampaign = (req, res, next) => {
     lower_inter_fee,
     google_cid,
     lower_memo,
-    _csrf,
-    media_count,
   } = req.body;
+
+  // for loop 위해서 array화 시키기
+  if (media_count < 2) {
+    media_id = [media_id];
+    media_start = [media_start];
+    media_end = [media_end];
+    lower_inter_type = [lower_inter_type];
+    lower_inter_name = [lower_inter_name];
+    lower_issue_date = [lower_issue_date];
+    lower_issue_type = [lower_issue_type];
+    lower_agency_fee_rate = [lower_agency_fee_rate];
+    lower_media_fee_rate = [lower_media_fee_rate];
+    lower_dplan_fee_rate = [lower_dplan_fee_rate];
+    lower_inter_fee_rate = [lower_inter_fee_rate];
+    lower_ad_fee = [lower_ad_fee];
+    lower_agency_fee = [lower_agency_fee];
+    lower_media_fee = [lower_media_fee];
+    lower_dplan_fee = [lower_dplan_fee];
+    lower_inter_fee = [lower_inter_fee];
+    google_cid = [google_cid];
+    lower_memo = [lower_memo];
+  }
+
   Team.findByPk(req.user.teamId)
     .then((team) => {
-      team.createCampaign({
-        type: cam_type,
-        pic: user_name,
-        title: cam_title,
-        agency: cam_agency,
-        period_begin: cam_start_date,
-        period_end: cam_end_date,
-        ad_fee: cam_ad_total,
-        agency_fee_rate: cam_agency_fee_rate,
-        agency_fee: cam_agency_fee,
-        tax_date: cam_tax_month,
-        issue_type: media_issue_type,
-        advertiserId: cam_advertiser_id,
-      });
+      team
+        .createCampaign({
+          type: cam_type,
+          pic: user_name,
+          title: cam_title,
+          agency: cam_agency,
+          period_begin: cam_start_date,
+          period_end: cam_end_date,
+          ad_fee: cam_ad_total,
+          agency_fee_rate: cam_agency_fee_rate,
+          agency_fee: cam_agency_fee,
+          tax_date: cam_tax_month,
+          issue_type: media_issue_type,
+          advertiserId: cam_advertiser_id,
+        })
+        .then((cam) => {
+          for (let i = 0; i < media_count; i++) {
+            cam
+              .createMediaItem({
+                mediumId: media_id[i],
+                media_start: media_start[i],
+                media_end: media_end[i],
+                inter_type: lower_inter_type[i],
+                inter_name: lower_inter_name[i],
+                issue_date: lower_issue_date[i],
+                issue_type: lower_issue_type[i],
+                agency_fee_rate: lower_agency_fee_rate[i],
+                media_fee_rate: lower_media_fee_rate[i],
+                dplan_fee_rate: lower_dplan_fee_rate[i],
+                inter_fee_rate: lower_inter_fee_rate[i],
+                ad_fee: lower_ad_fee[i],
+                agency_fee: lower_agency_fee[i],
+                media_fee: lower_media_fee[i],
+                dplan_fee: lower_dplan_fee[i],
+                inter_fee: lower_inter_fee[i],
+                google_cid: google_cid[i] === '' ? null : google_cid[i],
+                memo: lower_memo[i],
+              })
+              .catch((err) => {
+                return console.log(err);
+              });
+          }
+        })
+        .then(() => {
+          res.redirect("/");
+        })
+        .catch((err) => {
+          return console.log(err);
+        });
     })
     .catch((err) => {
       return console.log(err);
     });
-  res.redirect("/");
 };
