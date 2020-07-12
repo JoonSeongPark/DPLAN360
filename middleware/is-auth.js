f15f5995b01b8f270ab2a1ce067e8367bfda4554
JoonSeongPark <rytt@yonsei.ac.kr>
2020-07-12 23:21:23 +0900

    comment add

--- middleware/is-auth.js
+++ middleware/is-auth.js
@@ -1,3 +1,4 @@
+// 로그인 안 되어있을 경우, 로그인 페이지로 이동
 exports.isAuth = (req, res, next) => {
   if (!req.session.isLoggedIn) {
     return res.redirect("/login");
@@ -5,6 +6,7 @@ exports.isAuth = (req, res, next) => {
   next();
 };
 
+// 관리자 계정이 아닐 경우, Home 화면으로 이동
 exports.isAdmin = (req, res, next) => {
   if (!req.session.isAdmin) {
     return res.redirect("/");
@@ -12,6 +14,7 @@ exports.isAdmin = (req, res, next) => {
   next();
 };
 
+// 로그인 상태일 경우, 로그인 페이지 접근 불가
 exports.isLogin = (req, res, next) => {
   if (req.session.isLoggedIn) {
     return res.redirect("/");
