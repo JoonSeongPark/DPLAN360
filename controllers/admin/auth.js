dd5e14b412cbdf59959d2e651aa4518f1a7e6d5a
JoonSeongPark <rytt@yonsei.ac.kr>
2020-07-16 19:50:57 +0900

    user controller splited

--- controllers/admin/auth.js
+++ controllers/admin/auth.js
@@ -1,7 +1,6 @@
 const bcrypt = require("bcryptjs");
 
 const User = require("../../models/user");
-const Team = require("../../models/team");
 
 exports.getLogin = (req, res, next) => {
   let message = req.flash("error");
@@ -46,74 +45,6 @@ exports.postLogin = (req, res, next) => {
         req.flash("error", "비밀번호가 일치하지 않습니다.");
         res.redirect("/login");
       });
-    })
-    .catch((err) => {
-      return console.log(err);
-    });
-};
-
-exports.getUserSignup = (req, res, next) => {
-  let successMessage = req.flash("success");
-  if (successMessage.length > 0) successMessage = successMessage[0];
-  else successMessage = null;
-
-  let errorMessage = req.flash("error");
-  if (errorMessage.length > 0) errorMessage = errorMessage[0];
-  else errorMessage = null;
-
-  Team.findAll()
-    .then((teams) => {
-      res.render("auth/user-signup", {
-        pageTitle: "Add User",
-        menuTitle: "사용자 등록",
-        path: "/admin/user-signup",
-        teams: teams,
-        successMessage,
-        errorMessage,
-        isLoggedIn: req.session.isLoggedIn,
-      });
-    })
-    .catch((err) => {
-      return console.log(err);
-    });
-};
-
-exports.postUserSignup = (req, res, next) => {
-  const { teamId, name, email, leader } = req.body;
-
-  User.findOne({ where: { email: email } })
-    .then((user) => {
-      if (user) {
-        req.flash("error", "사용자가 이미 존재합니다.");
-        return res.redirect("/admin/user-signup");
-      }
-      bcrypt
-        .hash(email, 12)
-        .then((hashedPassword) => {
-          Team.findByPk(teamId)
-            .then((team) => {
-              team
-                .createUser({
-                  name,
-                  email,
-                  password: hashedPassword,
-                  leader,
-                })
-                .then((user) => {
-                  req.flash("success", "새로운 사용자가 생성되었습니다.");
-                  return res.redirect("/admin/user-signup");
-                })
-                .catch((err) => {
-                  return console.log(err);
-                });
-            })
-            .catch((err) => {
-              return console.log(err);
-            });
-        })
-        .catch((err) => {
-          return console.log(err);
-        });
     })
     .catch((err) => {
       return console.log(err);
