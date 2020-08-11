7f0d92d7c40775ac4a0cb1ca0867354e15667c0a
JoonSeongPark <rytt@yonsei.ac.kr>
2020-08-11 13:43:43 +0900

    Sequelize logging off

--- util/database.js
+++ util/database.js
@@ -3,6 +3,7 @@ const Sequelize = require("sequelize");
 const sequelize = new Sequelize("dplan360", "root", "Pjs895623!", {
   dialect: "mysql",
   host: "localhost",
+  logging: false,
 });
 
 module.exports = sequelize;
