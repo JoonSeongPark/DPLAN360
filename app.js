6ae9cc2893c3c8998f7bdaf4800e21a731965aca
JoonSeongPark <rytt@yonsei.ac.kr>
2020-09-14 17:19:01 +0900

    fix: solve isLoggedIn error

--- app.js
+++ app.js
@@ -84,6 +84,7 @@ app.use((req, res, next) => {
 });
 
 app.use((req, res, next) => {
+  res.locals.isLoggedIn = false;
   if (req.user) {
     res.locals.isLeader = req.user.leader;
     res.locals.blockAuth = req.user.block_auth;
